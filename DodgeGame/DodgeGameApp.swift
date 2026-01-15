c93a206cdc5faab8583f83ab2f39d467b36a9d4f
JeremyLih <jeremyli8776999@gmail.com>
2026-01-15 10:04:06 -0800

    Initial Commit

---
+++ DodgeGame/DodgeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DodgeGameApp.swift
+//  DodgeGame
+//
+//  Created by Jeremy Li on 2026-01-15.
+//
+
+import SwiftUI
+
+@main
+struct DodgeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
